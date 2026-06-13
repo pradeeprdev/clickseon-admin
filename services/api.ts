8a77b676d547edfceabde3cfea48e704017a7e82
Pradeep Rawat <pradeeprawat1717@gmail.com>
2026-06-13 22:34:14 +0530

    feat: update

--- services/api.ts
+++ services/api.ts
@@ -4,7 +4,7 @@ const API_BASE = process.env.NEXT_PUBLIC_API_BASE || 'http://localhost:5050/api'
 
 const instance = axios.create({
   baseURL: API_BASE,
-  timeout: 10000,
+  timeout: 100000,
   headers: { 'Content-Type': 'application/json' },
 });
 
